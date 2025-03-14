296ae85def909f425051cde489670a8c7f65a64f
rahil-code <mok865123@gmail.com>
2025-03-14 00:26:33 -0600

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Group_6_Flights_Application
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Group_6_Flights_Application" };
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Group_6_Flights_Application
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
